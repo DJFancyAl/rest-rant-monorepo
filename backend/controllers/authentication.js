8ba32ab8d51eef16e0e1ae62f02e09d56efb8006
DJFancyAl <abluemleiv@gmail.com>
2023-06-26 19:57:51 -0400

    Implemented JSON Web Tokens.

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,10 +1,11 @@
 const router = require('express').Router()
 const db = require("../models")
 const bcrypt = require('bcrypt')
+const jwt = require('json-web-token')
 
 const { User } = db
 
-  
+// Login  
 router.post('/', async (req, res) => {
     let user = await User.findOne({
         where: { email: req.body.email }
@@ -15,7 +16,36 @@ router.post('/', async (req, res) => {
             message: `Could not find a user with the provided username and password` 
         })
     } else {
-        res.json({ user })
+        const result = await jwt.encode(process.env.JWT_SECRET, { id: user.userId })
+        res.json({ user: user, token: result.value })
+    }
+})
+
+
+// Profile
+router.get('/profile', async (req, res) => {
+    try {
+        // Split the header into [ "Bearer", "TOKEN" ]
+        const [authenticationMethod, token] = req.headers.authorization.split(' ')
+
+        if (authenticationMethod == 'Bearer') {
+
+            // Decode the JWT
+            const result = await jwt.decode(process.env.JWT_SECRET, token)
+
+            // Get the logged in user's id from the payload
+            const { id } = result.value
+
+            // Find the user object using their id:
+            let user = await User.findOne({
+                where: {
+                    userId: id
+                }
+            })
+            res.json(user)
+        }
+    } catch {
+        res.json(null)
     }
 })
 
